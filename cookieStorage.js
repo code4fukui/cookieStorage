f731324765524d5cdfe46cd69bda090f711d9317
Taisuke Fukuno <fukuno@jig.jp>
2025-07-16 07:02:18 +0900

    Initial commit

---
+++ cookieStorage.js
@@ -0,0 +1,38 @@
+const simesite = "Strict";
+//const simesite = "Lax";
+
+export const cookieStorage = {
+  setItem: function (key, value, days = 7) {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    let cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}; expires=${expires}; path=/; SameSite=${simesite}`;
+    if (location.protocol === "https:") {
+      cookie += "; Secure";
+    }
+    document.cookie = cookie;
+  },
+  getItem: function (key) {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+      const [k, v] = cookie.split("=");
+      if (decodeURIComponent(k) === key) {
+        return decodeURIComponent(v);
+      }
+    }
+    return null;
+  },
+  removeItem: function (key) {
+    console.log("remm", key)
+    let cookie = `${encodeURIComponent(key)}=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; SameSite=${simesite}`;
+    if (location.protocol === "https:") {
+      cookie += "; Secure";
+    }
+    document.cookie = cookie;
+  },
+  clear: function () {
+    const cookies = document.cookie.split("; ");
+    for (const cookie of cookies) {
+      const [k] = cookie.split("=");
+      this.removeItem(decodeURIComponent(k));
+    }
+  },
+};
